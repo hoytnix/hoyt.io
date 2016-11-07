bae152b6e287cc48559379009d2be7e639fb55ff
hoytnix <hoyt.nix@gmail.com>
2016-11-06 19:29:48 -0500

    Database backup

--- database/backup.sql
+++ database/backup.sql
@@ -62,7 +62,7 @@ SELECT pg_catalog.setval('categories_id_seq', 2, true);
 
 INSERT INTO posts VALUES ('2016-10-29 10:39:10.875079+00', '2016-10-29 10:39:16.978281+00', 2, 1, 'Lorem ipsum', false, true, 3, '833b3b3cd89111201c8afbc3313efd45630d41a438f53a63dff1ddeece02c24889bd9f23208760bb0d7e6dacf6ec400991923ecdd9d215dbf9a4aee8047097cc');
 INSERT INTO posts VALUES ('2016-10-29 10:38:45.584668+00', '2016-11-01 16:56:48.845386+00', 1, 1, 'Hello world!', false, true, 2, '4bcec55d8bd4fbd16b9be02bf00922194a1526479c42c8b58a316a785d892bddad9b4ad128927c8265f6c5f057448ab031f2f27a724ecdd37004d297cc08e5b0');
-INSERT INTO posts VALUES ('2016-10-29 10:42:27.850815+00', '2016-11-01 17:14:30.669473+00', 3, 2, 'youtube-dl', true, true, 5, '06ef748d271b4c6419073d70724b04a9a909a227fb5b07192709b8c7afa4dba6f4389b85749360d56a37dea35ddfef1baa702dffd5ea0e8cc6861800ae0580d5');
+INSERT INTO posts VALUES ('2016-10-29 10:42:27.850815+00', '2016-11-07 00:26:05.344948+00', 3, 2, 'youtube-dl', true, true, 5, '06ef748d271b4c6419073d70724b04a9a909a227fb5b07192709b8c7afa4dba6f4389b85749360d56a37dea35ddfef1baa702dffd5ea0e8cc6861800ae0580d5');
 
 
 --
@@ -85,7 +85,7 @@ INSERT INTO tag VALUES (3, 'Desktop');
 -- Name: tag_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('tag_id_seq', 3, true);
+SELECT pg_catalog.setval('tag_id_seq', 6, true);
 
 
 --
