e2dc9b2b2436b7a543f235b2ca9dfe2653237bc0
hoytnix <hoyt.nix@gmail.com>
2016-11-17 21:50:17 -0500

    Posts are handled by the database instead of files

--- flask-server/hoyt/blueprints/blog/models.py
+++ flask-server/hoyt/blueprints/blog/models.py
@@ -4,6 +4,7 @@ import hashlib
 
 import pytz
 from flask import url_for
+from sqlalchemy import or_
 
 from hoyt.extensions import db
 from lib.urls import slugify
@@ -26,10 +27,28 @@ class Post(ResourceMixin, db.Model):
 
     # Details
     title = db.Column(db.String(128))
+    body = db.Column(db.Text)
     comments_enabled = db.Column(db.Boolean, default=True)
     is_published = db.Column(db.Boolean, default=False)
     preview_paragraphs = db.Column(db.Integer, default=2)
     file_hash = db.Column(db.String(128))
+
+    @classmethod
+    def search(cls, query):
+        """
+        Search a resource by 1 or more fields.
+
+        :param query: Search query
+        :type query: str
+        :return: SQLAlchemy filter
+        """
+        if not query:
+            return ''
+
+        search_query = '%{0}%'.format(query)
+        search_chain = (Post.title.ilike(search_query), )
+
+        return or_(*search_chain)
 
     @property
     def next_post(self):
@@ -64,45 +83,6 @@ class Post(ResourceMixin, db.Model):
         return current
 
     @property
-    def body(self):
-        """TODO: This method is illegal, it is a getter, and also causes effects.
-
-        1. Attempts to read a file of the same name of the post; if it can't it
-        will create one.
-
-        2. Creates a checksum of the file-contents, and if they do not match the
-        one stored in the database it will update it with the current datetime.
-
-        Returns:
-            str: contents of the post-file.
-        """
-
-        cd = os.path.dirname(os.path.abspath(__file__))
-        posts_dir = os.path.join(cd, 'posts')
-
-        file_name = '{}-{}.md'.format(self.id, self.slug)
-        fp = os.path.join(posts_dir, file_name)
-
-        if not os.path.exists(fp):
-            with open(fp, 'w+') as stream:
-                pass
-            return ''
-        else:
-            with open(fp, 'r') as stream:
-                text = stream.read()
-
-        h = hashlib.new('sha512')
-        h.update(text.encode('utf-8', errors='ignore'))
-        hash_sum = h.hexdigest()
-
-        if hash_sum != self.file_hash:
-            self.file_hash = hash_sum
-            self.updated_on = datetime.datetime.now(pytz.utc)
-            self.save()
-
-        return text
-
-    @property
     def preview_text(self):
         """Return so many paragraphs of the body, as specified by `preview_paragraphs`.
 
@@ -110,10 +90,12 @@ class Post(ResourceMixin, db.Model):
         """
 
         text = self.body
-        if text == '':
-            return text
-        paragraphs = text.split('\n\n')
-        return '\n\n'.join(paragraphs[:self.preview_paragraphs])
+        if not text or text == '':
+            return ''
+
+        paragraphs = text.split('\r\n\r\n')
+        return '\r\n\r\n'.join(paragraphs[:self.preview_paragraphs])
+        #return '%r' % text
 
     @property
     def category(self):
--- flask-server/hoyt/blueprints/dashboard/forms.py
+++ flask-server/hoyt/blueprints/dashboard/forms.py
@@ -1,4 +1,6 @@
 # pylint: disable=unused-import
+from collections import OrderedDict
+
 from flask_wtf import Form
 from wtforms import (SelectField, StringField, TextAreaField, BooleanField,
                      IntegerField, FloatField, DateTimeField)
@@ -6,8 +8,23 @@ from wtforms.validators import (DataRequired, Length, Optional, Regexp,
                                 NumberRange)
 from wtforms_alchemy import Unique
 
-from lib.util_wtforms import ModelForm
+from lib.util_wtforms import ModelForm, choices_from_dict
+
+
+class SearchForm(Form):
+    q = StringField('Search terms', [Optional(), Length(1, 256)])
+
+
+class BulkDeleteForm(Form):
+    SCOPE = OrderedDict([('all_selected_items', 'All selected items'),
+                         ('all_search_results', 'All search results')])
+
+    scope = SelectField(
+        'Privileges', [DataRequired()],
+        choices=choices_from_dict(
+            SCOPE, prepend_blank=False))
 
 
 class PostForm(ModelForm):
+    title = StringField('Title')
     body = TextAreaField('Body')
